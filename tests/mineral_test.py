7a35f2787d3b22a2effa8a5b84e4949b9b28646f
Heidi <heidiaclayton@yahoo.com>
2017-02-28 21:47:47 -0800

    Add test for indexOfGreaterThan()

--- tests/mineral_test.py
+++ tests/mineral_test.py
@@ -1,1 +1,37 @@
-# mineral classification tests to go here.
+#!/usr/bin/env python
+
+from pycoal import mineral
+
+def test_normalize():
+    pass
+
+def test_indexOfGreaterThan():
+    # test object
+    test_classifier = mineral.MineralClassification()
+
+    # [0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+    test_list = [x * 1.0 for x in range(10)]
+
+    assert test_classifier._MineralClassification__indexOfGreaterThan(test_list, 5) == 6
+
+    assert test_classifier._MineralClassification__indexOfGreaterThan(test_list, 1) == 2
+
+    assert test_classifier._MineralClassification__indexOfGreaterThan(test_list, 11) == 9
+
+def test_trainClassifier():
+    pass
+
+def test_saveClassifier():
+    pass
+
+def test_readClassifier():
+    pass
+
+def test_classifyPixel():
+    pass
+
+def test_classifyImage():
+    pass
+
+def test_classifyImages():
+    pass
