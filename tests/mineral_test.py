63793ab612d412781c1e05772723b339793cbc45
Taylor Alexander Brown <browtayl@oregonstate.edu>
2017-03-15 19:12:20 -0700

    remove obsolete tests

--- tests/mineral_test.py
+++ tests/mineral_test.py
@@ -14,54 +14,5 @@ from pycoal import mineral
 import spectral
 import numpy
 
-def test_normalize():
-    """
-    This tests the public method normalize in the class MineralClassification.
-    Args:
-        None
-    Returns:
-        None
-    """
-    obj = mineral.MineralClassification()
-    library = spectral.open_image("s06av95a_envi.hdr")
-
-    # TODO this causes a warning, change to concatenate
-    spectra = library.spectra[..., numpy.newaxis, ...]
-
-    normalized = obj.normalize(spectra, map(float, library.metadata.get('wavelength')))
-
-    assert normalized.shape == (1365, 128)
-
-
-def test_indexOfGreaterThan():
-    """
-    This tests the private method __indexOfGreaterThan in the class MineralClassification.
-    Args:
-        None
-    Returns:
-        None
-    """
-    test_list = [0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
-
-    # [0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
-    #                         ---  ---
-    # 6.0 is the first value greater than 5.0, so the return should be its index (6)
-    assert mineral.MineralClassification._MineralClassification__indexOfGreaterThan(test_list, 5.0) == 6
-
-    # [0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
-    #     ---  ---
-    # 2.0 is the first value greater than 1.0, so the return should be its index (2)
-    assert mineral.MineralClassification._MineralClassification__indexOfGreaterThan(test_list, 1.0) == 2
-
-    # [0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
-    # 11.0 is not in the range of this list, so the return will just be the last index
-    assert mineral.MineralClassification._MineralClassification__indexOfGreaterThan(test_list, 11.0) == 9
-
-def test_classifyPixel():
-    pass
-
 def test_classifyImage():
-    pass
-
-def test_classifyImages():
     pass
