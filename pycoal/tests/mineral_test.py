be7713bd30296ad665fd6bef7ae6714b561bd922
Taylor Alexander Brown <browtayl@oregonstate.edu>
2017-04-17 21:58:16 -0700

    test rgb converter

--- pycoal/tests/mineral_test.py
+++ pycoal/tests/mineral_test.py
@@ -10,9 +10,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pycoal import mineral
-import spectral
-import numpy
+from nose import with_setup
 
-def test_classifyImage():
-    pass
+import os
+import numpy
+import spectral
+import pycoal
+import pycoal.mineral
+
+# filename of 1x1 subimage
+test_toRGB_imageFilename = 'pycoal/tests/ang20150422t163638_corr_v1e_img_987_654.hdr'
+
+# filename of 1x1 RGB subimage
+test_toRGB_rgbFilename = 'pycoal/tests/ang20150422t163638_corr_v1e_img_987_654_rgb.hdr'
+
+# filenames of temporary files
+test_toRGB_testFilename = 'pycoal/tests/ang20150422t163638_corr_v1e_img_987_654_rgb_test.hdr'
+test_toRGB_testImage = 'pycoal/tests/ang20150422t163638_corr_v1e_img_987_654_rgb_test.img'
+
+# delete temporary files
+def _test_toRGB_teardown():
+    try:
+        os.remove(test_toRGB_testFilename)
+        os.remove(test_toRGB_testImage)
+    except OSError:
+        pass
+
+# verify that the RGB converter selects the correct bands and updates the metadata
+@with_setup(None, _test_toRGB_teardown)
+def test_toRGB():
+    pycoal.mineral.MineralClassification.toRGB(test_toRGB_imageFilename, test_toRGB_testFilename)
+    expected = spectral.open_image(test_toRGB_rgbFilename)
+    actual = spectral.open_image(test_toRGB_testFilename)
+    assert numpy.array_equal(expected.asarray(), actual.asarray())
+    assert expected.metadata.get('wavelength') == actual.metadata.get('wavelength')
+    assert expected.metadata.get('correction factors') == actual.metadata.get('correction factors')
+    assert expected.metadata.get('fwhm') == actual.metadata.get('fwhm')
+    assert expected.metadata.get('bbl') == actual.metadata.get('bbl')
+    assert expected.metadata.get('smoothing factors') == actual.metadata.get('smoothing factors')
