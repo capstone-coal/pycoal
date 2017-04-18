2f9a19aa9cc4c748a22c7520d9bff5eb6184b77f
Taylor Alexander Brown <browtayl@oregonstate.edu>
2017-04-18 03:40:38 -0700

    add method to filter unused classes

--- pycoal/tests/mineral_test.py
+++ pycoal/tests/mineral_test.py
@@ -10,9 +10,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pycoal import mineral
-import spectral
-import numpy
+from nose import with_setup
 
-def test_classifyImage():
-    pass
+import os
+import shutil
+import numpy
+import spectral
+import pycoal
+import pycoal.mineral
+
+# test files for filterClasses test
+test_filterClasses_Filename = 'pycoal/tests/ang20150420t182808_corr_v1e_img_class_4200-4210_70-80.hdr'
+test_filterClasses_Image = 'pycoal/tests/ang20150420t182808_corr_v1e_img_class_4200-4210_70-80.img'
+test_filterClasses_testFilename = 'pycoal/tests/ang20150420t182808_corr_v1e_img_class_4200-4210_70-80_filtered.hdr'
+test_filterClasses_testImage = 'pycoal/tests/ang20150420t182808_corr_v1e_img_class_4200-4210_70-80_filtered.img'
+
+# set up filterClasses test by copying classified image
+def _test_filterClasses_setup():
+    shutil.copyfile(test_filterClasses_Filename, test_filterClasses_testFilename)
+    shutil.copyfile(test_filterClasses_Image, test_filterClasses_testImage)
+
+# tear down filterClasses test by deleting filtered image
+def _test_filterClasses_teardown():
+    try:
+        os.remove(test_filterClasses_testFilename)
+        os.remove(test_filterClasses_testImage)
+    except OSError:
+        pass
+
+# verify that filterClasses removes unused classes and reindexes correctly
+@with_setup(_test_filterClasses_setup, _test_filterClasses_teardown)
+def test_filterClasses():
+    pycoal.mineral.MineralClassification.filterClasses(test_filterClasses_testFilename)
+    original = spectral.open_image(test_filterClasses_Filename)
+    filtered = spectral.open_image(test_filterClasses_testFilename)
+    assert int(filtered.metadata.get('classes')) == len(set(original.asarray().flatten().tolist()))
+    for x in range(original.shape[0]):
+        for y in range(original.shape[1]):
+            originalClassName = original.metadata.get('class names')[original[x,y,0]]
+            filteredClassName = filtered.metadata.get('class names')[filtered[x,y,0]]
+            assert originalClassName == filteredClassName
