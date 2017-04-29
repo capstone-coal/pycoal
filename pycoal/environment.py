9cae65631184d752893884c9fe76fb65acfe0d0e
Taylor Alexander Brown <browtayl@oregonstate.edu>
2017-04-29 05:16:34 -0700

    implement environmental correlation

--- pycoal/environment.py
+++ pycoal/environment.py
@@ -10,27 +10,133 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import pycoal
+from subprocess import call
+import spectral
+import numpy
+from os.path import abspath, dirname, basename, splitext
+import pycoal
 
-# class EnvironmentalCorrelation:
+class EnvironmentalCorrelation:
 
-#     def correlateMiningEnvironment(environmentFilename, miningFilename, outputFilename):
+    def __init__(self):
+        """
+        Construct a new ``EnvironmentalCorrelation`` object.
+        """
+        pass
 
-#         """
-#         Correlate environmental impact GIS data with classified mines and save the result to a file.
-#         """
+    def intersectProximity(self, miningFilename, vectorFilename, proximity, correlatedFilename):
+        """
+        Generate an environmental correlation image containing pixels from the
+        mining classified image detected within a given distance of features
+        within a vector layer.
 
-#         # define function to select the environmental impact layer
-#         def impactLayer(environmentDataset):
+        Args:
+            miningImage (str):     filename of the mining classified image
+            vectorLayer (str):     filename of vector layer
+            proximity (float):     distance in meters
+            correlatedImage (str): filename of the correlated image
+        """
 
-#             ???
+        # get path and file names
+        outputDirectory = dirname(abspath(correlatedFilename))
+        miningName = splitext(basename(abspath(miningFilename)))[0]
+        vectorName = splitext(basename(abspath(vectorFilename)))[0]
 
-#             return impactDataset
+        # rasterize the vector features to the same dimensions as the mining image
+        featureHeaderName = outputDirectory + '/' + miningName + '_' + vectorName + '.hdr'
+        self.createEmptyCopy(miningFilename, featureHeaderName)
+        featureImageName = featureHeaderName[:-4] + '.img'
+        self.rasterize(vectorFilename, featureImageName)
 
-#         # define raster function to correlate environmental impact with classified mines
-#         def correlate(impactDataset, miningDataset, outputDataset):
+        # generate a proximity map from the features
+        proximityHeaderName = outputDirectory + '/' + miningName + '_' + vectorName + '_proximity.hdr'
+        proximityImageName = proximityHeaderName[:-4] + '.img'
+        self.proximity(featureImageName, proximityImageName)
 
-#             ???
+        # intersect features within proximity
+        miningImage = spectral.open_image(miningFilename)
+        proximityImage = spectral.open_image(proximityHeaderName)
+        correlatedImage = numpy.zeros(shape=miningImage.shape, dtype=numpy.uint16)
+        for x in range(miningImage.shape[0]):
+            for y in range(miningImage.shape[1]):
+                if miningImage[x,y,0]==1 and proximityImage[x,y,0]<=proximity:
+                    correlatedImage[x,y,0] = miningImage[x,y,0]
 
-#         # apply the correlation and save the result
-#         pycoal.gis.GISProcessing.combine(correlate, impactLayer, environmentFilename, miningFilename, outputFilename)
+        # save the environmental correlation image
+        spectral.io.envi.save_classification(
+            correlatedFilename,
+            correlatedImage,
+            class_names=['No data','Data'],
+            metadata={
+                'data ignore value': 0,
+                'description': 'PyCOAL '+pycoal.version+' environmental correlation image.',
+                'map info': miningImage.metadata.get('map info')
+            })
+
+    def createEmptyCopy(self, sourceFilename, destinationFilename):
+        """
+        Create an empty copy of a PyCOAL classified image with the same size.
+
+        Args:
+            sourceFilename (str):      filename of the source image
+            destinationFilename (str): filename of the destination image
+        """
+
+        # open the source image
+        source = spectral.open_image(sourceFilename)
+
+        # create an empty array of the same dimensions
+        destination = numpy.zeros(shape=source.shape, dtype=numpy.uint16)
+
+        # save it with source metadata
+        spectral.io.envi.save_classification(
+            destinationFilename,
+            destination,
+            class_names=['No data','Data'],
+            metadata={
+                'data ignore value': 0,
+                'map info': source.metadata.get('map info')
+            })
+
+    def rasterize(self, vectorFilename, featureFilename):
+        """
+        Burn features from a vector image onto a raster image.
+
+        Args:
+            vectorFilename (str):  filename of the vector image
+            featureFilename (str): filename of the raster image
+        """
+
+        # assume the layer has the same name as the image
+        layerName = splitext(basename(vectorFilename))[0]
+
+        # convert vector features into nonzero pixels of the output file
+        returncode = call(['gdal_rasterize',
+                           '-burn', '1',
+                           '-l', layerName,
+                           vectorFilename,
+                           featureFilename])
+
+        # detect errors
+        if returncode != 0:
+            raise RuntimeError
+
+    def proximity(self, featureFilename, proximityFilename):
+        """
+        Generate a proximity map from the features.
+
+        Args:
+            featureFilename (str):   filename of the feature image
+            proximityFilename (str): filename of the proximity image
+        """
+
+        # generate an ENVI proximity map with georeferenced units
+        returncode = call(['gdal_proximity.py',
+                           featureFilename,
+                           proximityFilename,
+                           '-of', 'envi',
+                           '-distunits' ,'GEO'])
+
+        # detect errors
+        if returncode != 0:
+            raise RuntimeError
