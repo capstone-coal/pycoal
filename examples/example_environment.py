e930626e855bd2e8cd3452cfe4fd57f4e1829f05
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2017-07-04 11:10:29 -0700

    Fix landscape.io issues for example_environment.py

--- examples/example_environment.py
+++ examples/example_environment.py
@@ -1,26 +1,30 @@
 # Copyright (C) 2017 COAL Developers
 #
-# This program is free software; you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; version 2.
 #
-# This program is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty 
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public 
-# License along with this program; if not, write to the Free 
-# Software Foundation, Inc., 51 Franklin Street, Fifth 
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth
 # Floor, Boston, MA 02110-1301, USA.
 # encoding: utf-8
 '''
-example_environment -- an example script which demonstrates COAL environmental classification
+example_environment -- an example script which demonstrates COAL 
+environmental classification
 
-example_environment provides a CLI which demonstrates how the COAL Environmental Classification 
-API finds pixels in a mining classified image that are within a certain number of meters from 
-features in a vector layer such as flow lines in the National Hydrography Dataset (NHD).
-More reading an this example can be seen at https://capstone-coal.github.io/docs#usage
+example_environment provides a CLI which demonstrates how the 
+COAL Environmental Classification API finds pixels in a mining
+classified image that are within a certain number of meters
+from features in a vector layer such as flow lines in the
+National Hydrography Dataset (NHD).
+More reading an this example can be seen at 
+https://capstone-coal.github.io/docs#usage
 
 @author:     COAL Developers
 
@@ -48,6 +52,7 @@ DEBUG = 1
 TESTRUN = 0
 PROFILE = 0
 
+
 def run_environment(mining_filename, vector_filename, correlation_filename):
     # path to mining classified image
     mining_filename = "ang20150420t182050_corr_v1e_img_class_mining.hdr"
@@ -56,17 +61,21 @@ def run_environment(mining_filename, vector_filename, correlation_filename):
     vector_filename = "Shape/NHDFlowline.shp"
 
     # path to save environmental correlation image
-    correlation_filename = "ang20150420t182050_corr_v1e_img_class_mining_NHDFlowline_correlation.hdr"
+    correlation_filename = 
+        "ang20150420t182050_corr_v1e_img_class_mining_NHDFlowline_correlation.hdr"
 
     # create a new environmental correlation instance
     environmental_correlation = pycoal.environment.EnvironmentalCorrelation()
 
-    # generate an environmental correlation image of mining pixels within 10 meters of a stream
-    environmental_correlation.intersect_proximity(mining_filename, vector_filename, 10.0, correlation_filename)
+    # generate an environmental correlation image of mining
+    # pixels within 10 meters of a stream
+    environmental_correlation.intersect_proximity(
+        mining_filename, vector_filename, 10.0, correlation_filename)
 
-def main(argv=None): # IGNORE:C0111
+def main(argv=None):
     '''Command line options.'''
-    logging.basicConfig(filename='pycoal.log',level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(filename='pycoal.log', level=logging.INFO, 
+        format='%(asctime)s %(message)s')
     if argv is None:
         argv = sys.argv
     else:
@@ -80,18 +89,18 @@ def main(argv=None): # IGNORE:C0111
 
   Copyright 2017 COAL Developers. All rights reserved.
 
-  This program is free software; you can redistribute it and/or 
-  modify it under the terms of the GNU General Public License 
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
   as published by the Free Software Foundation; version 2.
 
-  This program is distributed in the hope that it will be useful, 
-  but WITHOUT ANY WARRANTY; without even the implied warranty 
-  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public 
-  License along with this program; if not, write to the Free 
-  Software Foundation, Inc., 51 Franklin Street, Fifth 
+  You should have received a copy of the GNU General Public
+  License along with this program; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin Street, Fifth
   Floor, Boston, MA 02110-1301, USA.
 
 USAGE
@@ -99,23 +108,38 @@ USAGE
 
     try:
         # Setup argument parser
-        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("-m", "--mining", dest="mining_filename", default='ang20150420t182050_corr_v1e_img_class_mining.hdr', help="Input mining classified file to be processed [default: ang20150420t182050_corr_v1e_img_class_mining.hdr]")
-        parser.add_argument("-hy", "--hydrography", dest="vector_filename", default='Shape/NHDFlowline.shp', help="Path to hydrography data [default: Shape/NHDFlowline.shp]")
-        parser.add_argument("-e", "--environment", dest="correlation_filename", default='ang20150420t182050_corr_v1e_img_class_mining_NHDFlowline_correlation.hdr', help="Output environmental correlation image [default: ang20150420t182050_corr_v1e_img_class_mining_NHDFlowline_correlation.hdr]")
+        parser = ArgumentParser(
+            description=program_license,
+            formatter_class=RawDescriptionHelpFormatter)
+        parser.add_argument(
+            "-m", "--mining",
+            dest="mining_filename",
+            default='ang20150420t182050_corr_v1e_img_class_mining.hdr',
+            help="Input mining classified file to be processed [default: ang20150420t182050_corr_v1e_img_class_mining.hdr]")
+        parser.add_argument(
+            "-hy", "--hydrography",
+            dest="vector_filename",
+            default='Shape/NHDFlowline.shp',
+            help="Path to hydrography data [default: Shape/NHDFlowline.shp]")
+        parser.add_argument(
+            "-e", "--environment",
+            dest="correlation_filename",
+            default='ang20150420t182050_corr_v1e_img_class_mining_NHDFlowline_correlation.hdr',
+            help="Output environmental correlation image [default: ang20150420t182050_corr_v1e_img_class_mining_NHDFlowline_correlation.hdr]")
 
         # Process arguments
-        args = parser.parse_args(['-m', 'ang20150420t182050_corr_v1e_img_class_mining.hdr', '-hy', 'Shape/NHDFlowline.shp', '-e', 'ang20150420t182050_corr_v1e_img_class_mining_NHDFlowline_correlation.hdr'])
-        #args = parser.parse_args()
+        args = parser.parse_args(
+            ['-m', 'ang20150420t182050_corr_v1e_img_class_mining.hdr',
+            '-hy', 'Shape/NHDFlowline.shp',
+            '-e', 'ang20150420t182050_corr_v1e_img_class_mining_NHDFlowline_correlation.hdr'])
 
         mining_filename = args.mining_filename
         vector_filename = args.vector_filename
         correlation_filename = args.correlation_filename
-        
+
         run_environment(mining_filename, vector_filename, correlation_filename)
 
     except KeyboardInterrupt:
-        ### handle keyboard interrupt ###
         return 0
     except Exception as e:
         if DEBUG or TESTRUN:
