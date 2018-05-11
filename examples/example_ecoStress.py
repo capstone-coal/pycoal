d83b86a0696528806c5e6429f76cd9e50cfa9cd1
Bryce Egley <begley@catalyte.io>
2018-05-11 10:01:31 -0700

    Lower case filename

--- examples/example_ecoStress.py
+++ examples/example_ecoStress.py
@@ -1,22 +1,22 @@
 #Copyright (C) 2017-2018 COAL Developers
 #
-# This program is free software; you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; version 2.
 #
-# This program is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty 
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public 
-# License along with this program; if not, write to the Free 
-# Software Foundation, Inc., 51 Franklin Street, Fifth 
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth
 # Floor, Boston, MA 02110-1301, USA.
 # encoding: utf-8
 
 '''
-example_ecoStress.py -- a script which will generate envi .sli and .hdr convolved library
+example_ecostress.py -- a script which will generate envi .sli and .hdr convolved library
 files of `ECOStress Spectral Library <https://speclib.jpl.nasa.gov/>`_
 
 Dependencies
@@ -68,5 +68,3 @@ for root, dir, files in os.walk(library_filename):
 
 ecoStress = mineral.AsterConversion()
 ecoStress.convert(library_filename,'dataEcoStress.db','ECO_01_SPLIB.envi')
-
-
