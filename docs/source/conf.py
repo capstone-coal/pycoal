f0efd3325e8e4769d30a88af168ea14d76727c0e
mjn0898 <mujahidn@usc.edu>
2019-10-21 08:43:04 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../pycoal'))
+sys.path.insert('../../')
+sys.path.insert('../../../')
 import guzzle_sphinx_theme
 
 # -- General configuration ------------------------------------------------
